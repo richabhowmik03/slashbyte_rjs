c786353d4cf9e58abc9f213ce2cf9958112c7350
Richa Bhowmik <93598610+richabhowmik03@users.noreply.github.com>
2025-07-14 23:05:39 +0530

    Updated api.py

--- backend/api.py
+++ backend/api.py
@@ -215,4 +215,4 @@ async def http_exception_handler(request, exc):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(api, host="0.0.0.0", port=8000)
